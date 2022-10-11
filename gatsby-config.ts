a11b8f478e22c038c8bd2809cb0a1651c9b8b21b
Kha Le <minhkhanb@gmail.com>
2022-10-11 10:25:55 +0700

    adding sass

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,35 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "123"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: '123',
+      },
     },
-    __key: "images"
-  }]
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    `gatsby-plugin-sass`,
+  ],
 };
 
 export default config;
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+
+const Layout: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <p className='text-gray'>paragraph</p>
+    </div>
+  );
+};
+
+export default Layout;
